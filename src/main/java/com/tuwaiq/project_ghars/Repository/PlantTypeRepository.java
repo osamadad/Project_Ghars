ad26f29b994bc3c8c0462647870696b491fd1ea0
osama dad <osamadad768@gmail.com>
2025-12-26 18:05:53 +0300

    feat: plant class

---
+++ src/main/java/com/tuwaiq/project_ghars/Repository/PlantTypeRepository.java
@@ -0,0 +1,11 @@
+package com.tuwaiq.project_ghars.Repository;
+
+import com.tuwaiq.project_ghars.Model.Address;
+import com.tuwaiq.project_ghars.Model.PlantType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlantTypeRepository extends JpaRepository<PlantType,Integer> {
+    PlantType findPlantTypeById(Integer id);
+}
