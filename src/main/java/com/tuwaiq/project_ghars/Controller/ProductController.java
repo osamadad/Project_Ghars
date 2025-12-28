b8db3da5bfd4551442ac68b2563ed6c18f345d16
osama dad <osamadad768@gmail.com>
2025-12-28 10:20:20 +0300

    feat: store filtering endpoints

--- src/main/java/com/tuwaiq/project_ghars/Controller/ProductController.java
+++ src/main/java/com/tuwaiq/project_ghars/Controller/ProductController.java
@@ -2,10 +2,12 @@ package com.tuwaiq.project_ghars.Controller;
 
 import com.tuwaiq.project_ghars.Api.ApiResponse;
 import com.tuwaiq.project_ghars.Model.Product;
+import com.tuwaiq.project_ghars.Model.User;
 import com.tuwaiq.project_ghars.Service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -41,5 +43,20 @@ public class ProductController {
     public ResponseEntity<?> deleteProduct(@PathVariable Integer userId, @PathVariable Integer productId) {
         productService.deleteProduct(userId, productId);
         return ResponseEntity.status(200).body(new ApiResponse("Product deleted successfully"));
+    }
+
+    @GetMapping("/by-sell-type/{sellType}")
+    public ResponseEntity<?> getProductsBySellType(@AuthenticationPrincipal User user, @PathVariable String sellType) {
+        return ResponseEntity.status(200).body(productService.getProductsBySellType(user.getId(), sellType));
+    }
+
+    @GetMapping("/order-by-price")
+    public ResponseEntity<?> getProductsOrderByPrice(@AuthenticationPrincipal User user) {
+        return ResponseEntity.status(200).body(productService.getProductsOrderByPriceAsc(user.getId()));
+    }
+
+    @GetMapping("/price-range/{minPrice}/{maxPrice}")
+    public ResponseEntity<?> getProductsByPriceRange(@AuthenticationPrincipal User user, @PathVariable Integer minPrice, @PathVariable Integer maxPrice) {
+        return ResponseEntity.status(200).body(productService.getProductsByMinAndMaxPrice(user.getId(), minPrice, maxPrice));
     }
 }
