e2ffaacf6718efe02ac4966266e871825390bbc5
osama dad <osamadad768@gmail.com>
2025-12-30 22:55:44 +0300

    feat: fixed service test

--- src/main/java/com/tuwaiq/project_ghars/DTOIn/AddProductDTOIn.java
+++ src/main/java/com/tuwaiq/project_ghars/DTOIn/AddProductDTOIn.java
@@ -1,9 +1,11 @@
 package com.tuwaiq.project_ghars.DTOIn;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AddProductDTOIn {
 
     @NotEmpty
